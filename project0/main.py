30b5e5d58dc3547d53dc8e97bf1d4e1103dcfcf8
Archit Mittal <e19cse041@gmail.com>
2024-10-02 19:35:25 -0400

    url remove

--- project0/main.py
+++ project0/main.py
@@ -111,7 +111,6 @@ def fetchFromUrl(url):
         if(url is None):
             return None
         url = f"{url}"
-        print(url)
         headers ={}
         headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
         data = urllib.request.urlopen(urllib.request.Request(url=url, headers=headers)).read()
